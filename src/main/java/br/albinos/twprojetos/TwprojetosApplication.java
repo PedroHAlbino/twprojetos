974fbb4a03948b04ce720772d4a369219bee980b
PedroAlbino <pedro.albino@consulticom.com.br>
2023-01-02 21:29:16 -0300

    Git Add

---
+++ src/main/java/br/albinos/twprojetos/TwprojetosApplication.java
@@ -0,0 +1,13 @@
+package br.albinos.twprojetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwprojetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwprojetosApplication.class, args);
+	}
+
+}
